387679f72ffdf7b30ddc6c3705aa35c3e2e73178
James Cuzella <james.cuzella@lyraphase.com>
2020-08-04 12:02:50 -0600

    Update pcloud-drive to version 3.9.4 (Fixes #4)

--- Casks/pcloud-drive.rb
+++ Casks/pcloud-drive.rb
@@ -1,12 +1,12 @@
 cask 'pcloud-drive' do
-  version '3.8.4'
-  sha256 'c5c29785d6584d5c35da340e01687dc479949bce10cd7545ffef6b1f0e18bcd0'
+  version '3.9.4'
+  sha256 '05e6ed62185d689688894096e0b9c32ee9256b77d4f4e1503d60dbfec1b5c593'
 
   url do
     require 'net/http'
     require 'json'
     api = 'https://api.pcloud.com/'
-    code = 'XZYOy4kZNOeMXcp5GkFAEbmG8Uh5eVl35j4k'
+    code = 'XZxCdakZRh9qTwDrTU57A4KXw5E2CVMnLrDV'
     uri = URI(api + 'getpublinkdownload?code=' + code)
     response = Net::HTTP.get(uri)
     data = JSON.parse(response)
@@ -17,7 +17,7 @@ cask 'pcloud-drive' do
 
   depends_on cask: 'osxfuse'
 
-  pkg 'pCloud Drive 3.8.4.pkg'
+  pkg 'pCloud Drive 3.9.4.pkg'
 
   uninstall quit:    'com.pcloud.pcloud.macos',
             pkgutil: 'com.mobileinno.pkg.pCloudDrive'
