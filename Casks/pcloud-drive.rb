ec59f6a075916ae03da6197ac820e1bfc20fe463
James Cuzella <james.cuzella@lyraphase.com>
2020-08-04 12:22:48 -0600

    Update to GPLv3 license

--- Casks/pcloud-drive.rb
+++ Casks/pcloud-drive.rb
@@ -1,3 +1,26 @@
+#
+# Cask:: pcloud-drive
+# Recipe:: default
+# Authors:: James Cuzella, Tom Gross
+#
+# Copyright:: © 🄯 2019-2020,  James Cuzella
+# Copyright:: © 🄯 2019 Tom Gross
+# License:: GPLv3.0
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 cask "pcloud-drive" do
   version "3.9.4"
   sha256 "05e6ed62185d689688894096e0b9c32ee9256b77d4f4e1503d60dbfec1b5c593"
