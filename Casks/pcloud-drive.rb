d49fe20db4e3ab1d334564f26dce30e36476bbc2
James Cuzella <james.cuzella@lyraphase.com>
2020-08-04 12:08:10 -0600

    Fix Style//StringLiterals RuboCop 💩

--- Casks/pcloud-drive.rb
+++ Casks/pcloud-drive.rb
@@ -1,24 +1,24 @@
-cask 'pcloud-drive' do
-  version '3.9.4'
-  sha256 '05e6ed62185d689688894096e0b9c32ee9256b77d4f4e1503d60dbfec1b5c593'
+cask "pcloud-drive" do
+  version "3.9.4"
+  sha256 "05e6ed62185d689688894096e0b9c32ee9256b77d4f4e1503d60dbfec1b5c593"
 
   url do
-    require 'net/http'
-    require 'json'
-    api = 'https://api.pcloud.com/'
-    code = 'XZxCdakZRh9qTwDrTU57A4KXw5E2CVMnLrDV'
-    uri = URI(api + 'getpublinkdownload?code=' + code)
+    require "net/http"
+    require "json"
+    api = "https://api.pcloud.com/"
+    code = "XZxCdakZRh9qTwDrTU57A4KXw5E2CVMnLrDV"
+    uri = URI(api + "getpublinkdownload?code=" + code)
     response = Net::HTTP.get(uri)
     data = JSON.parse(response)
-    data['hosts'][0] + data['path']
+    data["hosts"][0] + data["path"]
   end
-  name 'pCloud Drive'
-  homepage 'https://www.pcloud.com/'
+  name "pCloud Drive"
+  homepage "https://www.pcloud.com/"
 
-  depends_on cask: 'osxfuse'
+  depends_on cask: "osxfuse"
 
-  pkg 'pCloud Drive 3.9.4.pkg'
+  pkg "pCloud Drive 3.9.4.pkg"
 
-  uninstall quit:    'com.pcloud.pcloud.macos',
-            pkgutil: 'com.mobileinno.pkg.pCloudDrive'
+  uninstall quit:    "com.pcloud.pcloud.macos",
+            pkgutil: "com.mobileinno.pkg.pCloudDrive"
 end
